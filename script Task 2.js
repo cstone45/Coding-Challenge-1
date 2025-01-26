58f3f01a1b6f35310cf02df96c16765f42b35b1f
cstone45 <cstone45@usf.edu>
2025-01-26 01:56:53 -0500

    Task 2 - Product Details

---
+++ script Task 2.js
@@ -0,0 +1,7 @@
+// Task 2: Primitive Data Types
+let productName = "Hersheys Bar"; //String
+console.log(typeof String(productName)); // Output: Hersheys Bar
+const productPrice = $3 // Number
+console.log(typeof Number(productPrice)); // Output: $3
+var isAvalible = "true" 
+console.log(typeof Boolean(true)); // Output: true
