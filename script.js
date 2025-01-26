4f0ec2bc2a97f3d063bea84e90add5c019d4e3fe
cstone45 <cstone45@usf.edu>
2025-01-26 01:38:38 -0500

    Task 1 - Employee Information

---
+++ script.js
@@ -0,0 +1,7 @@
+// Task 1: Variable Declaration
+let employeeName = "Dexter"; // String
+console.log(typeof String (employeeName)); // Output: Dexter
+const employeeID = 4012; // Number
+console.log(typeof Number(employeeID)); // Output: 4012
+var isActive = "true"
+console.log(typeof Boolean(true)); // Output: true
