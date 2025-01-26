a4e949e07c04608a382ed1ebd25701452de5ec6a
cstone45 <cstone45@usf.edu>
2025-01-26 03:45:01 -0500

    Conner Stones Coding Challenge 1 Javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+// Task 1: Variable Declaration
+let employeeName = "Dexter"; // String
+console.log(typeof String (employeeName)); // Output: Dexter
+const employeeID = 4012; // Number
+console.log(typeof Number(employeeID)); // Output: 4012
+var isActive = "true"; // Boolean
+console.log(typeof Boolean(true)); // Output: true
+
+//Task 2: Primitive Data Types
+let productName = "Hersheys Bar"; //String
+console.log(typeof String(productName)); // Output: Hersheys Bar
+const productPrice = $3 // Number
+console.log(typeof Number(productPrice)); // Output: $3
+var isAvalible = "true" 
+console.log(typeof Boolean(true)); // Output: true
+
+// Task 3: Number Data Types
+let accountBalance = $50; // Number
+total = $50 + $25;
+$75 - $6;
+$69 * 2;
+let total = $138 / 2;
+console.log(total);
+
+// Task 4: String Data Type
+let customerName = "Dave,"; // String
+let message = "Welcome to Hersheys Chocolate! We're glad you're here!"
+console.log(customerName + message); // Output: Dave, Welcome to Hersheys Chocolaye! We're glad you're here!
+
+// Task 5: Boolean Data Type
+let isLoggedIn = "true"
