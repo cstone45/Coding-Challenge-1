a003f2db3ceef296ce23c0542fa478a3cdc5d1b6
cstone45 <cstone45@usf.edu>
2025-01-26 01:51:02 -0500

    Task 2 - Product Details

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ let employeeName = "Dexter"; // String
 console.log(typeof String (employeeName)); // Output: Dexter
 const employeeID = 4012; // Number
 console.log(typeof Number(employeeID)); // Output: 4012
-var isActive = "true"
+var isActive = "true"; // Boolean
+console.log(typeof Boolean(true)); // Output: true
+
+//Task 2: Primitive Data Types
+let productName = "Hersheys Bar"; //String
+console.log(typeof String(productName)); // Output: Hersheys Bar
+const productPrice = $3 // Number
+console.log(typeof Number(productPrice)); // Output: $3
+var isAvalible = "true" 
 console.log(typeof Boolean(true)); // Output: true
